37603a682d18d8cbbb747ffb33854a274e51e1de
dangluyen20 <62934817+dangluyen20@users.noreply.github.com>
2020-07-11 09:59:09 +0700

    Bai23

---
+++ BaiTapTKWeb/Bai23/java/Bai23.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$(Link).click(function (e) { 
+        var b = confirm("Are you sure want to goto that page");
+         if(b)
+         {
+             Di();  // đưa đến trang: ntu.edu.vn
+         }
+    });
+});
