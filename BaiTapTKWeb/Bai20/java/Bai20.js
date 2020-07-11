7f3ef9a0a0a92d02c1138777fb3a49decb01b5aa
dangluyen20 <62934817+dangluyen20@users.noreply.github.com>
2020-07-11 09:58:40 +0700

    Bai20

---
+++ BaiTapTKWeb/Bai20/java/Bai20.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(bt1).click(function () { 
+         alert('1');
+     });
+     $(bt2).click(function() {
+         alert('2');
+     });
+     $(bt3).click(function () { 
+         alert('3');
+     });
+ });
